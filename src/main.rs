9f8c8e86a288e1295e66cde4d9ae55506918f5db
Zura Benashvili <zura.benashvili@gmail.com>
2023-02-07 21:23:35 +0400

    feat: add snark worker stats rpcs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::{hash_map::Entry, HashMap},
+    collections::{hash_map::Entry, HashMap, VecDeque},
     sync::Arc,
     time::{Duration, Instant},
 };
@@ -29,6 +29,184 @@ struct LockJobQueryParams {
     timeout: Option<u16>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "kind")]
+enum SnarkWorkerJobGetError {
+    NoAvailableJob,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "kind")]
+enum SnarkWorkerStatsPut {
+    JobGetInit {
+        time: u64,
+    },
+    JobGetError {
+        time: u64,
+        error: SnarkWorkerJobGetError,
+    },
+    JobGetSuccess {
+        time: u64,
+        ids: String,
+    },
+    WorkCreateError {
+        time: u64,
+        ids: String,
+        error: String,
+    },
+    WorkCreateSuccess {
+        time: u64,
+        ids: String,
+    },
+    WorkSubmitError {
+        time: u64,
+        ids: String,
+        error: String,
+    },
+    WorkSubmitSuccess {
+        time: u64,
+        ids: String,
+    },
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "kind")]
+enum SnarkWorkerState {
+    JobGetPending {
+        job_get_init_t: u64,
+    },
+    JobGetError {
+        job_get_init_t: u64,
+        job_get_error_t: u64,
+        error: SnarkWorkerJobGetError,
+    },
+    WorkCreatePending {
+        job_get_init_t: u64,
+        job_get_success_t: u64,
+        ids: String,
+    },
+    WorkCreateError {
+        job_get_init_t: u64,
+        job_get_success_t: u64,
+        work_create_error_t: u64,
+        ids: String,
+        error: String,
+    },
+    WorkSubmitPending {
+        job_get_init_t: u64,
+        job_get_success_t: u64,
+        work_create_success_t: u64,
+        ids: String,
+    },
+    WorkSubmitError {
+        job_get_init_t: u64,
+        job_get_success_t: u64,
+        work_create_success_t: u64,
+        work_submit_error_t: u64,
+        ids: String,
+        error: String,
+    },
+    WorkSubmitSuccess {
+        job_get_init_t: u64,
+        job_get_success_t: u64,
+        work_create_success_t: u64,
+        work_submit_success_t: u64,
+        ids: String,
+    },
+}
+
+impl SnarkWorkerState {
+    fn init(time: u64) -> Self {
+        Self::JobGetPending {
+            job_get_init_t: time,
+        }
+    }
+
+    fn apply(&mut self, v: SnarkWorkerStatsPut) -> bool {
+        match self.clone() {
+            Self::JobGetPending { job_get_init_t } => {
+                *self = match v {
+                    SnarkWorkerStatsPut::JobGetError { time, error } => Self::JobGetError {
+                        job_get_init_t,
+                        job_get_error_t: time,
+                        error,
+                    },
+                    SnarkWorkerStatsPut::JobGetSuccess { time, ids } => Self::WorkCreatePending {
+                        job_get_init_t,
+                        job_get_success_t: time,
+                        ids,
+                    },
+                    _ => return false,
+                }
+            }
+            Self::WorkCreatePending {
+                job_get_init_t,
+                job_get_success_t,
+                ids: expected_ids,
+            } => {
+                *self = match v {
+                    SnarkWorkerStatsPut::WorkCreateError {
+                        time, error, ids, ..
+                    } if ids == expected_ids => Self::WorkCreateError {
+                        job_get_init_t,
+                        job_get_success_t,
+                        work_create_error_t: time,
+                        ids,
+                        error,
+                    },
+                    SnarkWorkerStatsPut::WorkCreateSuccess { time, ids } if ids == expected_ids => {
+                        Self::WorkSubmitPending {
+                            job_get_init_t,
+                            job_get_success_t,
+                            work_create_success_t: time,
+                            ids,
+                        }
+                    }
+                    _ => return false,
+                };
+            }
+            Self::WorkSubmitPending {
+                job_get_init_t,
+                job_get_success_t,
+                work_create_success_t,
+                ids: expected_ids,
+            } => {
+                *self = match v {
+                    SnarkWorkerStatsPut::WorkSubmitError {
+                        time, error, ids, ..
+                    } if ids == expected_ids => Self::WorkSubmitError {
+                        job_get_init_t,
+                        job_get_success_t,
+                        work_create_success_t,
+                        work_submit_error_t: time,
+                        ids,
+                        error,
+                    },
+                    SnarkWorkerStatsPut::WorkSubmitSuccess { time, ids } if ids == expected_ids => {
+                        Self::WorkSubmitSuccess {
+                            job_get_init_t,
+                            job_get_success_t,
+                            work_create_success_t,
+                            work_submit_success_t: time,
+                            ids,
+                        }
+                    }
+                    _ => return false,
+                };
+            }
+            _ => return false,
+        }
+
+        true
+    }
+}
+
+impl Default for SnarkWorkerState {
+    fn default() -> Self {
+        Self::JobGetPending { job_get_init_t: 0 }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let opts = Opts::from_args();
@@ -37,8 +215,22 @@ async fn main() {
     let max_key_len = opts.max_key_len;
 
     let table = Arc::new(Mutex::new(HashMap::new()));
+    let worker_stats = Arc::new(Mutex::new(HashMap::new()));
+
     let kv = table.clone();
-    let server = warp::path!("lock-job" / String)
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(Duration::from_secs(2)).await;
+
+            let mut kv = kv.lock().await;
+            let now = Instant::now();
+            kv.retain(|_, t| *t > now);
+            drop(kv);
+        }
+    });
+
+    let kv = table.clone();
+    let lock_job_put = warp::path!("lock-job" / String)
         .and(warp::put())
         .and(
             warp::filters::query::query::<LockJobQueryParams>()
@@ -66,17 +258,66 @@ async fn main() {
             }
         });
 
-    let kv = table.clone();
-    tokio::spawn(async move {
-        loop {
-            tokio::time::sleep(Duration::from_secs(2)).await;
+    let stats = worker_stats.clone();
+    let worker_stats_put = warp::path!("worker-stats" / String)
+        .and(warp::put())
+        .and(warp::filters::body::json())
+        .then(move |pub_key: String, req: SnarkWorkerStatsPut| {
+            let stats = stats.clone();
+            async move {
+                let mut stats = stats.lock().await;
+                match stats.entry(pub_key) {
+                    Entry::Vacant(v) => match req {
+                        SnarkWorkerStatsPut::JobGetInit { time } => {
+                            let mut val = VecDeque::new();
+                            val.push_front(SnarkWorkerState::init(time));
+                            v.insert(val);
+                        }
+                        req => {
+                            let err = format!(
+                                "unexpected worker_stats/put\nstate: None\nrequest: {:?}",
+                                req
+                            );
+                            eprintln!("{}", err);
+                            return with_status(err, StatusCode::from_u16(400).unwrap());
+                        }
+                    },
+                    Entry::Occupied(v) => {
+                        let v = v.into_mut();
+                        match req {
+                            SnarkWorkerStatsPut::JobGetInit { time } => {
+                                v.push_front(SnarkWorkerState::init(time));
+                            }
+                            req => {
+                                if !v.front_mut().unwrap().apply(req.clone()) {
+                                    let err = format!(
+                                        "unexpected worker_stats/put\nstate: {:?}\nrequest: {:?}",
+                                        v, req
+                                    );
+                                    eprintln!("{}", err);
+                                    return with_status(err, StatusCode::from_u16(400).unwrap());
+                                }
+                            }
+                        }
+                    }
+                }
+                with_status("".to_owned(), StatusCode::from_u16(200).unwrap())
+            }
+        });
 
-            let mut kv = kv.lock().await;
-            let now = Instant::now();
-            kv.retain(|_, t| *t > now);
-            drop(kv);
-        }
-    });
+    let stats = worker_stats.clone();
+    let worker_stats_get = warp::path!("worker-stats")
+        .and(warp::get())
+        .then(move || {
+            let stats = stats.clone();
+            async move {
+                let stats = stats.lock().await;
+                with_status(serde_json::to_string(&*stats).unwrap(), StatusCode::from_u16(200).unwrap())
+            }
+        });
 
-    warp::serve(server).run(([0, 0, 0, 0], opts.port)).await;
+    let routes = lock_job_put
+        .or(worker_stats_put)
+        .or(worker_stats_get);
+    warp::serve(routes).run(([0, 0, 0, 0], opts.port)).await;
 }
